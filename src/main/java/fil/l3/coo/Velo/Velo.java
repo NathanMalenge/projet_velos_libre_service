1995037a01020c16bd78951e4b6a7244fb9e42f7
malenge <nathan.malenge.etu@univ-lille.fr>
2025-09-19 11:47:45 +0200

    attribut basket et baggage remontés dans la classe abstraite velo

--- src/main/java/fil/l3/coo/Velo/Velo.java
+++ src/main/java/fil/l3/coo/Velo/Velo.java
@@ -4,10 +4,21 @@ public abstract class Velo {
     private static int nextId = 1;
     private String id;
     private boolean isAvailable;
+    protected boolean hasBasket;
+    protected boolean hasBaggage;
 
     public Velo() {
         this.id = "VELO_" + nextId++;
         this.isAvailable = true;
+        this.hasBasket = false;
+        this.hasBaggage = false;
+    }
+
+    public Velo(boolean hasBasket, boolean hasBaggage) {
+        this.id = "VELO_" + nextId++;
+        this.isAvailable = true;
+        this.hasBasket = hasBasket;
+        this.hasBaggage = hasBaggage;
     }
 
     public String getId() {
@@ -20,6 +31,33 @@ public abstract class Velo {
 
     public void setAvailable(boolean available) {
         isAvailable = available;
+    }
+
+    public boolean hasBasket() {
+        return hasBasket;
+    }
+
+    public void setBasket(boolean hasBasket) {
+        this.hasBasket = hasBasket;
+    }
+
+    public boolean hasBaggage() {
+        return hasBaggage;
+    }
+
+    public void setBaggage(boolean hasBaggage) {
+        this.hasBaggage = hasBaggage;
+    }
+
+    protected double getAccessoriesPrice() {
+        double price = 0;
+        if (hasBasket) {
+            price += 0.5;
+        }
+        if (hasBaggage) {
+            price += 0.3;
+        }
+        return price;
     }
 
     public abstract double getPrice();
